619ce35a962893f0c9bb43fc16e6aef7f2cbbf16
Matiaspepi <matiaspepi24@gmail.com>
2022-04-10 13:10:31 -0300

    interacturar con DOM

---
+++ productos.js
@@ -0,0 +1,12 @@
+const productos = [
+
+
+    {
+        id: 1 ,
+        img: 'imagenes/productos/paleta1-bis.png',
+        nombre: "Padel Straviv TRiton Soft 21",
+        precio: 61.000,
+        boton: "Comprar",
+
+    }
+]
